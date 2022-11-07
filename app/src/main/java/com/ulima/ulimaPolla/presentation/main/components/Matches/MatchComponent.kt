c437ec6eefcca15417932943a03485fe19ff9ad4
Mauricio <mauriciocisneroslaule@gmail.com>
2022-11-06 22:26:46 -0500

    Input de Match Component

--- app/src/main/java/com/ulima/ulimaPolla/presentation/main/components/Matches/MatchComponent.kt
+++ app/src/main/java/com/ulima/ulimaPolla/presentation/main/components/Matches/MatchComponent.kt
@@ -3,17 +3,24 @@ package com.ulima.ulimaPolla.presentation.main.components.Matches
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import com.ulima.ulimaPolla.model.entity.Match
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MatchComponent(match : Match){
+    val keyboardController = LocalSoftwareKeyboardController.current
     var apuesta = remember {
         mutableStateOf("")
     }
@@ -25,10 +32,14 @@ fun MatchComponent(match : Match){
             TextMatch(match = match)
         }
         Column(modifier = Modifier.weight(2.0f)) {
-            OutlinedTextField(value = apuesta.value , onValueChange = {
-                apuesta.value = it
-            },
-            label = {Text("Apuesta")})
+            OutlinedTextField(value = apuesta.value ,
+                onValueChange = { apuesta.value = it },
+                label = {Text("Apuesta")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {keyboardController?.hide()})
+            )
         }
     }
 }
