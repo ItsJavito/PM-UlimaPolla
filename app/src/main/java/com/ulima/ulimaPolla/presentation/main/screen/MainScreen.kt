c2ba69d91adace9aa22058d06c5df5088a03fc04
Javier <javierenriqueos@gmail.com>
2022-11-06 22:16:28 -0500

    facilito

--- app/src/main/java/com/ulima/ulimaPolla/presentation/main/screen/MainScreen.kt
+++ app/src/main/java/com/ulima/ulimaPolla/presentation/main/screen/MainScreen.kt
@@ -1,11 +1,15 @@
 package com.ulima.ulimaPolla.presentation.main.screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ulima.ulimaPolla.presentation.main.components.ListaTeams
 import com.ulima.ulimaPolla.presentation.main.viewmodels.MainViewModel
 
@@ -16,9 +20,23 @@ fun MainScreen(
     LaunchedEffect(key1 = true){
         vm.getTeams()
     }
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+                .padding(top=20.dp)
+        ){
+            Text(
+                text = "Equipos",
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp
+            )
+        }
         ListaTeams(teams = vm.listaTeams)
         //Row fantasma - Row Muerto - Row
     }
