f2bca3901673e41d6ba194977a89e95cc852a11b
Javier <javierenriqueos@gmail.com>
2022-11-07 14:18:59 -0500

    xdd

--- app/src/main/java/com/ulima/ulimaPolla/presentation/main/components/scaffold/BottomScaffold.kt
+++ app/src/main/java/com/ulima/ulimaPolla/presentation/main/components/scaffold/BottomScaffold.kt
@@ -20,13 +20,22 @@ import androidx.navigation.NavHostController
 fun BottomScaffold(
     navController: NavHostController,
 ){
-    val isSelected = remember {
+    val isSelectedEquipos = remember {
+        mutableStateOf(true)
+    }
+    val isSelectedPolla = remember {
         mutableStateOf(false)
+    }
+
+    var colorEquipos = remember {
+        mutableStateOf(Color.DarkGray)
+    }
+    var colorPolla = remember {
+        mutableStateOf(Color.LightGray)
     }
     Row(
         modifier = Modifier.fillMaxWidth()
-            .height(100.dp)
-            .background(Color.LightGray),
+            .height(100.dp),
     ) {
         //Box 1
         Box(
@@ -34,8 +43,19 @@ fun BottomScaffold(
             modifier = Modifier.weight(1f)
                 .fillMaxHeight()
                 .padding(0.dp)
+                .background(colorEquipos.value)
                 .clickable {
-                    navController.navigate("ListaEquipos");
+                    //si estaba en la pantalla de polla
+                    if(isSelectedPolla.value){
+                        isSelectedPolla.value = false;
+                        isSelectedEquipos.value = true;
+                        colorEquipos.value = Color.DarkGray
+                        colorPolla.value = Color.LightGray
+                        navController.navigate("ListaEquipos");
+                    }
+                    else if(isSelectedEquipos.value){
+                        // no hace nada, y no hagas nada porfa
+                    }
                 }
                 .border(1.dp, Color.DarkGray)
         ){
@@ -51,8 +71,19 @@ fun BottomScaffold(
             modifier = Modifier.weight(1f)
                 .fillMaxHeight()
                 .padding(0.dp)
+                .background(colorPolla.value)
                 .clickable {
-                    navController.navigate("matches")
+                    //si estaba en la pantalla de equipos
+                    if(isSelectedEquipos.value){
+                        isSelectedPolla.value = true;
+                        isSelectedEquipos.value = false;
+                        colorPolla.value = Color.DarkGray
+                        colorEquipos.value = Color.LightGray
+                        navController.navigate("matches")
+                    }
+                    else if(isSelectedPolla.value){
+                        // no hace nada, y no hagas nada porfa
+                    }
                 }
                 .border(1.dp, Color.DarkGray)
         ){
